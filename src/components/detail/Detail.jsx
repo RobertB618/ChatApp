6513ca995a57d011d07dd80e465d9f5390cbcb17
Robert <robert969alvis@gmail.com>
2025-01-05 16:15:44 +0200

    chat changes
    chat changes

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,14 @@ import "./detail.css";
 
 const Detail = () => {
   return (
-    <div className="detail">detail</div>
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>jane doe</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur</p>
+      </div>
+      <div className="info"></div>
+    </div>
   );
 };
 
